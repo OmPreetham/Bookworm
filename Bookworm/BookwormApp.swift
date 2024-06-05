0bdb8ba0d6a327f79e15d59d18e0ecd1d1fce618
Om Preetham <ompreethambandi@gmail.com>
2024-06-05 15:05:11 -0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Om Preetham Bandi on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
