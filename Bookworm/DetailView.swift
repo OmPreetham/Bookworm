0a7d12ba04d96f7d43488794ba9efb506610c851
Om Preetham <ompreethambandi@gmail.com>
2024-06-07 09:48:59 -0500

    added book view and delete functionality

---
+++ Bookworm/DetailView.swift
@@ -0,0 +1,78 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Om Preetham Bandi on 6/7/24.
+//
+
+import SwiftData
+import SwiftUI
+
+struct DetailView: View {
+    @Environment(\.modelContext) var modelContext
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var showingDeleteAlert = false
+    
+    let book: Book
+    
+    var body: some View {
+        ScrollView {
+            ZStackLayout(alignment: .bottomTrailing) {
+                Image(book.genre)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                    .offset(x: -5, y: -5)
+            }
+            
+            Text(book.author)
+                .font(.title)
+                .foregroundStyle(.secondary)
+
+            Text(book.review)
+                .padding()
+
+            RatingView(rating: .constant(book.rating), label: "")
+                .font(.largeTitle)
+        }
+        .alert("Delete book", isPresented: $showingDeleteAlert) {
+            Button("Delete", role: .destructive, action: deleteBook)
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Are you sure?")
+        }
+        .navigationTitle(book.title)
+        .scrollBounceBehavior(.basedOnSize)
+        .toolbar {
+            Button("Delete", systemImage: "trash") {
+                showingDeleteAlert.toggle()
+            }
+        }
+    }
+    
+    func deleteBook() {
+        modelContext.delete(book)
+    }
+}
+
+#Preview {
+    do {
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Book.self, configurations: config)
+        let example = Book(title: "Test Book", author: "Test Author", genre: "Fantasy", review: "This was a great book; I really enjoyed it.", rating: 4)
+
+        return DetailView(book: example)
+            .modelContainer(container)
+        
+    } catch {
+        return Text("Failed to create preview: \(error.localizedDescription)")
+    }
+}
